bd47caaac140a869cb85d3a68bbdc5ea6be62ead
SEDY_NP <ehls1234@naver.com>
2021-04-26 11:50:55 +0900

    result[0] == null, Modify for message output

--- Commands/weatherTaki.js
+++ Commands/weatherTaki.js
@@ -24,11 +24,7 @@ module.exports = {
             const API_URL = `https://api.openweathermap.org/data/2.5/onecall`
             // ?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}`
 
-            let cityname = result[0].location.name
-            let latitude = result[0].location.lat
-            let longitude = result[0].location.long
-
-            if (cityname == null) {
+            if (result[0] == null) {
                 return message.reply (
                     new Discord.MessageEmbed()
                         .setTitle("올바른 시를 입력해주세요")
@@ -36,6 +32,10 @@ module.exports = {
                         .addField("사용법","!내일날씨 <시/도>")
                 )
             }
+
+            let cityname = result[0].location.name
+            let latitude = result[0].location.lat
+            let longitude = result[0].location.long
 
             axios.get(API_URL, {
                 params: {
