ae9b6cd09b709c216519ea2225270e64a960b9cd
SEDY_NP <ehls1234@naver.com>
2021-04-27 15:04:03 +0900

    Modify content the same as 'nextDay'

--- Commands/weather.js
+++ Commands/weather.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "오늘날씨",
+    name: "내일날씨",
     execute(message,args){
         const weatherjs = require('weather-js')
         const Discord = require('discord.js')
@@ -9,22 +9,26 @@ module.exports = {
         if(args[0] == null) return message.reply(new Discord.MessageEmbed().setTitle("올바른 시를 입력해주세요").setColor("#ff5858")
         .addField("사용법","!내일날씨 <시/도>"))
 
-        weatherjs.find({search: args.join(" "), degreeType: 'C'},function(err,result){
-            if (err) {
-                return message.reply (
-                    new Discord.MessageEmbed()
-                        .setTitle("올바른 시를 입력해주세요")
-                        .setColor("#ff5858")
-                        .addField("사용법","!내일날씨 <시/도>")
-                )
-            }
-            console.log(result[0])
+            //console.log(result[0])
 
+        const naverGeo = {
+            headers: {
+                'X-NCP-APIGW-API-KEY-ID': 'oulqcr7gqi',
+                'X-NCP-APIGW-API-KEY': 'gywZuBAAs9J1Qo5NzxaMY5N4i0mDMjyregJQlvtM'
+            },
+            params: {
+                "query": args.join(" ")
+            }
+        }
+            
+        axios
+            .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", naverGeo)
+            .then((response) => {
             const API_KEY = "c13f40978e5aeaf76792cac87a6b3de6"
             const API_URL = `https://api.openweathermap.org/data/2.5/onecall`
             // ?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}`
 
-            if (result[0] == null) {
+            if (response.data == null) {
                 return message.reply (
                     new Discord.MessageEmbed()
                         .setTitle("올바른 시를 입력해주세요")
@@ -33,20 +37,21 @@ module.exports = {
                 )
             }
 
-            let cityname = result[0].location.name
-            let latitude = result[0].location.lat
-            let longitude = result[0].location.long
+            let cityname = response.data.addresses[0].roadAddress
+            let latitude = response.data.addresses[0].y
+            let longitude = response.data.addresses[0].x
 
-            axios.get(API_URL, {
-                params: {
-                   lat: latitude,
-                   lon: longitude,
-                   excludes: 'hourly',
-                   appid: API_KEY,
-                   units: 'metric'
-                }
-            })
-            .then(res => {
+            axios
+                .get(API_URL, {
+                    params: {
+                        lat: latitude,
+                        lon: longitude,
+                        excludes: 'hourly',
+                        appid: API_KEY,
+                        units: 'metric'
+                    }
+                })
+                .then(res => {
                 // 호출이 성공적으로 되면 res
                 //console.log(res.data.daily[1])
                 const toDay = res.data.daily[0]
@@ -73,10 +78,14 @@ module.exports = {
                         .setFooter('Openweathermap By SEDY', 'https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png')
                 )
             })
-            .catch(err => {
-                // 호출이 실패하면 err
-                console.error(err)
-            })
+                .catch(err => {
+                // 오픈웨더 호출이 실패하면 err
+                    console.error(err)
+                })
         })
+            .catch((error) => {
+                // 네이버 호출이 실패하면 err
+                console.log(error.response)
+            })
     }
 }
