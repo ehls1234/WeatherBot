bbff7a4a103c20d840b304653c7e61dea3bb625d
SEDY_NP <ehls1234@naver.com>
2021-05-12 13:52:48 +0900

    try, catch로 각 axios 에러를 잡게 수정

--- Commands/weatherTaki.js
+++ Commands/weatherTaki.js
@@ -1,55 +1,71 @@
 module.exports = {
     name: "내일날씨",
     async execute(message,args){
-            const Discord = require('discord.js')
-            const axios = require('axios').default
-            const {stripIndents} = require('common-tags')
-        
-            if(args[0] == null) return message.reply(new Discord.MessageEmbed().setTitle("올바른 시를 입력해주세요").setColor("#ff5858")
-            .addField("사용법","!내일날씨 <시/도>"))
-        
-            const naverGeo = {
-                headers: {
-                    'X-NCP-APIGW-API-KEY-ID': 'oulqcr7gqi',
-                    'X-NCP-APIGW-API-KEY': 'gywZuBAAs9J1Qo5NzxaMY5N4i0mDMjyregJQlvtM'
-                },
-                params: {
-                    "query": args.join(" ")
-                }
+        const Discord = require('discord.js')
+        const axios = require('axios').default
+        const {stripIndents} = require('common-tags')
+    
+        if(args[0] == null) return message.reply(new Discord.MessageEmbed().setTitle("올바른 시를 입력해주세요").setColor("#ff5858")
+        .addField("사용법","!내일날씨 <시/도>"))
+    
+        const naverGeo = {
+            headers: {
+                'X-NCP-APIGW-API-KEY-ID': 'oulqcr7gqi',
+                'X-NCP-APIGW-API-KEY': 'gywZuBAAs9J1Qo5NzxaMY5N4i0mDMjyregJQlvtM'
+            },
+            params: {
+                "query": args.join(" ")
             }
-                
-        const naverAxios = await axios
-            .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", naverGeo)
+        }
+        let cityname = null
+        let latitude = null
+        let longitude = null
+        try{
+            const naverAxios = await axios
+                .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", naverGeo)
+            
+            if (naverAxios.data.addresses[0] == null) {
+                return message.reply (
+                    new Discord.MessageEmbed()
+                    .setTitle("알수없는 주소 입니다.")
+                    .setColor("#ff5858")
+                    .addField("사용법","!오늘날씨 이름이 아닌 정확한 주소를 입력해 주세요.")
+                )
+            }
+        
+            cityname = naverAxios.data.addresses[0].roadAddress
+            latitude = naverAxios.data.addresses[0].y
+            longitude = naverAxios.data.addresses[0].x
+            }
+        catch(naverAxiosErr){
+            console.log(naverAxiosErr)
+            return message.reply("naverMaps API를 받아 올 수 없습니다.")
+        }
+
         const API_KEY = "c13f40978e5aeaf76792cac87a6b3de6"
         const API_URL = `https://api.openweathermap.org/data/2.5/onecall`
         // ?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}`
-        
-        if (naverAxios.data.addresses[0] == null) {
-            return message.reply (
-                new Discord.MessageEmbed()
-                .setTitle("알수없는 주소 입니다.")
-                .setColor("#ff5858")
-                .addField("사용법","!오늘날씨 이름이 아닌 정확한 주소를 입력해 주세요.")
-            )
+        let nextDay = null        
+        try{
+            const openWeatherMapAxios = await axios
+                .get(API_URL, {
+                    params: {
+                        lat: latitude,
+                        lon: longitude,
+                        excludes: 'hourly',
+                        appid: API_KEY,
+                        units: 'metric'
+                    }
+                })
+            // 호출이 성공적으로 되면 openWeatherMapAxios
+            //console.log(openWeatherMapAxios.data.daily[1])
+            nextDay = openWeatherMapAxios.data.daily[1]
         }
-    
-        let cityname = naverAxios.data.addresses[0].roadAddress
-        let latitude = naverAxios.data.addresses[0].y
-        let longitude = naverAxios.data.addresses[0].x
-                    
-        const openWeatherMapAxios = await axios
-            .get(API_URL, {
-                params: {
-                    lat: latitude,
-                    lon: longitude,
-                    excludes: 'hourly',
-                    appid: API_KEY,
-                    units: 'metric'
-                }
-            })
-        // 호출이 성공적으로 되면 openWeatherMapAxios
-        //console.log(openWeatherMapAxios.data.daily[1])
-        const nextDay = openWeatherMapAxios.data.daily[1]
+        catch(openWeatherMapAxiosErr){
+            console.log(openWeatherMapAxiosErr)
+            return message.reply("openWeatherMap API를 받아 올 수 없습니다.")
+        }
+
         let iconcode = nextDay.weather[0].icon;
         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         
