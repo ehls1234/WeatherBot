50f7ac2b19cc371dd610cb805a6aae294d7490b5
SEDY_NP <ehls1234@naver.com>
2021-05-12 13:23:11 +0900

    try, catch를 각 API에 설정해주어 에러를 잡게 수정

--- Commands/weather.js
+++ Commands/weather.js
@@ -17,24 +17,32 @@ module.exports = {
                 "query": args.join(" ")
             }
         }
-        
-        const naverAxios = await axios
-            .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", naverGeo)
-        const API_KEY = "c13f40978e5aeaf76792cac87a6b3de6"
-        const API_URL = `https://api.openweathermap.org/data/2.5/onecall`
-        // ?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}`
-        if (naverAxios.data.addresses[0] == null) {
-            return message.reply (
-                new Discord.MessageEmbed()
-                .setTitle("알수없는 주소 입니다.")
-                .setColor("#ff5858")
-                .addField("사용법","!오늘날씨 이름이 아닌 정확한 주소를 입력해 주세요.")
-            )
+
+        let cityname = null
+        let latitude = null
+        let longitude = null
+
+        try{
+            const naverAxios = await axios
+                .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", naverGeo)
+            if (naverAxios.data.addresses[0] == null) {
+                return message.reply (
+                    new Discord.MessageEmbed()
+                    .setTitle("알수없는 주소 입니다.")
+                    .setColor("#ff5858")
+                    .addField("사용법","!오늘날씨 이름이 아닌 정확한 주소를 입력해 주세요.")
+                )
+            }
+
+            cityname = naverAxios.data.addresses[0].roadAddress
+            latitude = naverAxios.data.addresses[0].y
+            longitude = naverAxios.data.addresses[0].x
+        }
+        catch(naverErr){
+            console.log(naverErr)
+            return message.reply("naverMaps API 오류 입니다.")
         }
 
-        let cityname = naverAxios.data.addresses[0].roadAddress
-        let latitude = naverAxios.data.addresses[0].y
-        let longitude = naverAxios.data.addresses[0].x
         let guName = cityname.slice().trim().split(/ +/)   //args[1]
 
         let dustToDay = new Date()
@@ -47,71 +55,91 @@ module.exports = {
         let dustDay = `${dustYear}-${dustMonth}-${dustDate}`
 
         let concentration10 = null, concentration25 = null, pm10Value = null, pm25Value = null, isDustProcessed = false
-        
-        const dustAxios = await axios 
-            .get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty`,{
-                params: {
-                    serviceKey: "zoALXhvtGbPAtIKDwmsVk5KDDsO+aA7Y1CkDwLfdoxYk/3WHjJ68bvl27cvh+NOscS/uYHVspUWS+VgoIvr/Aw==",
-                    stationName: guName[1],
-                    returnType: "json",
-                    dataTerm : "DAILY",
-                    ver: "1.3"
 
+        try{
+            const dustAxios = await axios 
+                .get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty`,{
+                    params: {
+                        serviceKey: "zoALXhvtGbPAtIKDwmsVk5KDDsO+aA7Y1CkDwLfdoxYk/3WHjJ68bvl27cvh+NOscS/uYHVspUWS+VgoIvr/Aw==",
+                        stationName: guName[1],
+                        returnType: "json",
+                        dataTerm : "DAILY",
+                        ver: "1.3"
+
+                    }
+                })
+            if (
+                guName != null && guName.length > 1 &&
+                dustAxios.data.response.header.resultCode === '00' &&
+                dustAxios.data.response.body.totalCount > 0
+            ) {
+                const dustData = dustAxios.data.response.body.items.shift()
+                const { pm10Grade, pm25Grade } = dustData
+                pm10Value = dustData.pm10Value
+                pm25Value = dustData.pm25Value
+                console.log([pm10Value, pm25Value, pm10Grade, pm25Grade])
+                const grades = ['좋음', '보통', '나쁨', '매우나쁨']
+                // 1 : 좋음, 2: 보통, 3: 나쁨, 4: 매우나쁨
+            
+                if(pm25Value <= 15){
+                    concentration25 = grades[0]}
+                else if(pm25Value <= 35){
+                    concentration25 = grades[1]
+                }else if(pm25Value <= 75){
+                    concentration25 = grades[2]
+                }else{
+                    concentration25 = grades[3]
                 }
-            })
-        if (
-            guName != null && guName.length > 1 &&
-            dustAxios.data.response.header.resultCode === '00' &&
-            dustAxios.data.response.body.totalCount > 0
-        ) {
-            const dustData = dustAxios.data.response.body.items.shift()
-            const { pm10Grade, pm25Grade } = dustData
-            pm10Value = dustData.pm10Value
-            pm25Value = dustData.pm25Value
-            console.log([pm10Value, pm25Value, pm10Grade, pm25Grade])
-            const grades = ['좋음', '보통', '나쁨', '매우나쁨']
-            // 1 : 좋음, 2: 보통, 3: 나쁨, 4: 매우나쁨
-        
-            if(pm25Value <= 15){
-                concentration25 = grades[0]}
-            else if(pm25Value <= 35){
-                concentration25 = grades[1]
-            }else if(pm25Value <= 75){
-                concentration25 = grades[2]
-            }else{
-                concentration25 = grades[3]
-            }
 
-            if(pm10Value <= 30){
-                concentration10 = grades[0]
-            }else if(pm10Value <= 80) {
-                concentration10 = grades[1]
-            }else if(pm10Value <= 150) {
-                concentration10 = grades[2]
-            }else{
-                concentration10 = grades[3]
-            }
+                if(pm10Value <= 30){
+                    concentration10 = grades[0]
+                }else if(pm10Value <= 80) {
+                    concentration10 = grades[1]
+                }else if(pm10Value <= 150) {
+                    concentration10 = grades[2]
+                }else{
+                    concentration10 = grades[3]
+                }
 
-            console.log([concentration10, concentration25])
-            isDustProcessed = true
+                console.log([concentration10, concentration25])
+                isDustProcessed = true
+            }
         }
-        
-        const openWeatherMapAxios = await axios
-            .get(API_URL, {
-                params: {
-                    lat: latitude,
-                    lon: longitude,
-                    excludes: 'hourly',
-                    appid: API_KEY,
-                    units: 'metric'
-                }
-            })
+        catch(dustAxiosErr){
+            console.log(dustAxiosErr)
+        }
 
-        // 호출이 성공적으로 되면 openWeatherMapAxios
-        //console.log(openWeatherMapAxios.data.daily[1])
-        const nextDay = openWeatherMapAxios.data.daily[1]
-        const toDay = openWeatherMapAxios.data.current
-        const toDayDaily = openWeatherMapAxios.data.daily[0]
+        const API_KEY = "c13f40978e5aeaf76792cac87a6b3de6"
+        const API_URL = `https://api.openweathermap.org/data/2.5/onecall`
+        // ?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}`
+        
+        let nextDay = null
+        let toDay = null
+        let toDayDaily = null
+
+        try{
+            const openWeatherMapAxios = await axios
+                .get(API_URL, {
+                    params: {
+                        lat: latitude,
+                        lon: longitude,
+                        excludes: 'hourly',
+                        appid: API_KEY,
+                        units: 'metric'
+                    }
+                })
+
+            // 호출이 성공적으로 되면 openWeatherMapAxios
+            //console.log(openWeatherMapAxios.data.daily[1])
+            nextDay = openWeatherMapAxios.data.daily[1]
+            toDay = openWeatherMapAxios.data.current
+            toDayDaily = openWeatherMapAxios.data.daily[0]
+        }
+        catch(openWeatherMapAxiosErr){
+            console.log(openWeatherMapAxiosErr)
+            return message.reply("openWeatherMap API 오류 입니다.")
+        }
+
         let iconcode = toDay.weather[0].icon;
         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
        
@@ -182,8 +210,8 @@ module.exports = {
             `}`,inline: false}
             ]}
         }
-        catch(err){
-            console.log(err)
+        catch(dustForecastAxiosErr){
+            console.log(dustForecastAxiosErr)
         }
 
         const embed = new Discord.MessageEmbed()
