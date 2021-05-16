e695987bb080064593a9cd6bb363fce674bbaddc
SEDY_NP <ehls1234@naver.com>
2021-05-16 22:51:57 +0900

    코드 정리,순서 변경

--- Commands/bote.js
+++ Commands/bote.js
@@ -7,7 +7,6 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
     
         let userFields = []
-        let userIdSet = new Set()
         let boteCount = args[1]
 
         try{
@@ -27,84 +26,95 @@ module.exports = {
             🖐 = 저요!
             💌 = 인원 호출(모집시작자)
             ❌ = 종료`}`)
-        }}
+            }
+        }
         catch(err){
             console.error(err)
         }
-        const sendEmbed = await message.channel.send(embed)
+        try{
+            const sendEmbed = await message.channel.send(embed)
 
-        sendEmbed.react("🖐")
-        sendEmbed.react("💌")
-        sendEmbed.react("❌")
-
-        const meFilter = (reaction, user) => reaction.emoji.name === "🖐" && user.bot === false
-        const hereFilter = (reaction, user) => reaction.emoji.name === "💌" && user.id === message.author.id
-        const stopFilter = (reaction, user) => reaction.emoji.name === "❌" && user.id === message.author.id
-        
-        const stop = sendEmbed.createReactionCollector(stopFilter, {time: 900000, dispose: true})
-        const me = sendEmbed.createReactionCollector(meFilter, {time: 900000, dispose: true})
-        const here = sendEmbed.createReactionCollector(hereFilter, {time: 900000, dispose: true})
-    
-        //stop
-        stop.on("collect",async r => {
-            await message.delete()
-            embed.setColor("FF0000")
-            embed.setTitle(`"${args[0]}" 모집이 종료 되었습니다.`)
-            await sendEmbed.reactions.removeAll()
-            embed.setDescription(`${stripIndents`
-            모집 인원이 전부 찼습니다.
-            💌 = 인원 호출(모집시작자)
-            `}`)
-            sendEmbed.edit(embed)
+            sendEmbed.react("🖐")
             sendEmbed.react("💌")
-            return message.channel.send(`"${args[0]}"모집이 종료 되었습니다.`)
-        })
+            sendEmbed.react("❌")
 
-    
-        let mentions = []
+            const meFilter = (reaction, user) => reaction.emoji.name === "🖐" && user.bot === false
+            const hereFilter = (reaction, user) => reaction.emoji.name === "💌" && user.id === message.author.id
+            const stopFilter = (reaction, user) => reaction.emoji.name === "❌" && user.id === message.author.id
 
-        me.on("collect",async r => {
+            const stop = sendEmbed.createReactionCollector(stopFilter, {time: 900000, dispose: true})
+            const me = sendEmbed.createReactionCollector(meFilter, {time: 900000, dispose: true})
+            const here = sendEmbed.createReactionCollector(hereFilter, {time: 900000, dispose: true})
             
-            for(let user of r.users.cache.values()){
-                if(user.bot == true){
-                }else if(mentions.length == boteCount){
-                    return user.send(`${user}"인원이 다차서 마감 되었습니다."`)
-                }else if(userIdSet.has(user.id)){
-                }else{
-                    userFields.push(
-                    {name:"사용자",value: `${stripIndents`
-                    ${user.username}
-                    `}`,inline: true}
-                    )
-                    embed.fields = userFields
-                    mentions.push(user)
-                    userIdSet.add(user.id)
-                }
-            }
-            if(mentions.length == boteCount){
-                sendEmbed.reactions.removeAll()
+            //stop
+            stop.on("collect",async r => {
+                await message.delete()
+                embed.setColor("FF0000")
+                embed.setTitle(`"${args[0]}" 모집이 종료 되었습니다.`)
+                await sendEmbed.reactions.removeAll()
                 embed.setDescription(`${stripIndents`
                 모집 인원이 전부 찼습니다.
                 💌 = 인원 호출(모집시작자)
-                ❌ = 종료`}`)
+                `}`)
+                sendEmbed.edit(embed)
                 sendEmbed.react("💌")
-                sendEmbed.react("❌")
-            }
-            sendEmbed.edit(embed)
-        })
+                return message.channel.send(`"${args[0]}"모집이 종료 되었습니다.`)
+            })
 
-        here.on("collect",async r => {
-            r.users.remove(message.author.id)
-            let mentionString = ``
-            if(mentions.length == 0){
-                return message.channel.send(`모집대상이 없습니다.`)
-            }
-            for(let u = 0; u < mentions.length; u++){
-                    mentionString+= `${mentions[u]}`
-            }
-            message.channel.send(`${mentionString}`)
-            message.channel.send(`모집대상 ${mentions.length}명 호출 했습니다.`)
-            sendEmbed.edit(embed)   
-        })
+        
+            let mentions = []
+            let userIdSet = new Set()
+
+            me.on("collect",async r => {
+               /* const arrayTest = Object.fromEntries(r.users.cache.entries())
+                let arrayTestKey = Object.keys(arrayTest)
+                const arrayTestValue = Object.entries(arrayTest)
+                console.log(arrayTest)
+                console.log(arrayTestValue)*/
+                for(let user of r.users.cache.values()){
+                    if(user.bot == true){
+                    }else if(mentions.length == boteCount){
+                        return user.send(`${user}"인원이 다차서 마감 되었습니다."`)
+                    }else if(userIdSet.has(user.id)){
+                    }else{
+                        userFields.push(
+                        {name:"사용자",value: `${stripIndents`
+                        ${user.username}
+                        `}`,inline: true}
+                        )
+                        embed.fields = userFields
+                        mentions.push(user)
+                        userIdSet.add(user.id)
+                    }
+                }
+                if(mentions.length == boteCount){
+                    sendEmbed.reactions.removeAll()
+                    embed.setDescription(`${stripIndents`
+                    모집 인원이 전부 찼습니다.
+                    💌 = 인원 호출(모집시작자)
+                    ❌ = 종료`}`)
+                    sendEmbed.react("💌")
+                    sendEmbed.react("❌")
+                }
+                sendEmbed.edit(embed)
+            })
+
+            here.on("collect",async r => {
+                r.users.remove(message.author.id)
+                let mentionString = ``
+                if(mentions.length == 0){
+                    return message.channel.send(`모집대상이 없습니다.`)
+                }
+                for(let u = 0; u < mentions.length; u++){
+                        mentionString+= `${mentions[u]}`
+                }
+                message.channel.send(`${mentionString}`)
+                message.channel.send(`모집대상 ${mentions.length}명 호출 했습니다.`)
+                sendEmbed.edit(embed)   
+            })
+        }
+        catch(emojiErr){
+            console.error(emojiErr)
+        }
     }
 }
